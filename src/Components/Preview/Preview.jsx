b1b90ba0be2092a63bd78f87000ef72b7f85bb6f
IsleBeeGun <IsleBeeGun@gmail.com>
2020-02-27 23:10:24 +0300

    Add slider

--- src/Components/Preview/Preview.jsx
+++ src/Components/Preview/Preview.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 import "./Preview.scss";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 export class Preview extends React.Component {
   render() {
-    return <div className="preview-container">Slider is here</div>
+    const settings = {
+      dots: true,
+      dotsClass: "control-dots",
+      arrows: false,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
+    return (
+      <div className="preview-container">
+        <Slider ref={c => (this.slider = c)} {...settings}>
+          {this.props.images.regular.warm.map((img_url, counter) => {
+            return <img alt="" src={img_url} key={"img-" + counter}/>;
+          })}
+        </Slider>
+      </div>
+    );
   }
 }
