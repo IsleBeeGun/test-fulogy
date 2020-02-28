daa8eddfbf5d1a3b0cfb29d799527a8901773f4d
IsleBeeGun <IsleBeeGun@gmail.com>
2020-02-28 20:37:17 +0300

    Adjust layout & Make datafile options.json

--- src/Components/Navigator/Navigator.jsx
+++ src/Components/Navigator/Navigator.jsx
@@ -3,6 +3,17 @@ import "./Navigator.scss";
 
 export class Navigator extends React.Component {
   render() {
-    return <nav className="navigator-container">I am navigator</nav>
+    return (
+      <nav className="navigator-container">
+        <button className="option-button">1</button>
+        <button className="option-button">2</button>
+        <button className="option-button">3</button>
+        <button className="option-button">4</button>
+        <button className="option-button">5</button>
+        <button className="option-button">6</button>
+        <button className="option-button">7</button>
+        <button className="option-button">8</button>
+      </nav>
+    );
   }
 }
--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import './index.css';
 import {App} from './Components/App/App';
 import * as serviceWorker from './serviceWorker';
 const images = require("./images.json");
+const options = require("./options.json");
 
-ReactDOM.render(<App images={images}/>, document.getElementById('root'));
+ReactDOM.render(<App images={images} options={options}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
