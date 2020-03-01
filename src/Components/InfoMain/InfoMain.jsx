f9afef180d8d028c8667b5c35982e6bedcf579a3
IsleBeeGun <IsleBeeGun@gmail.com>
2020-03-01 19:52:45 +0300

    npm install --save @fortawesome/fontawesome-free  (for some icons)

--- src/Components/InfoMain/InfoMain.jsx
+++ src/Components/InfoMain/InfoMain.jsx
@@ -6,7 +6,7 @@ export class InfoMain extends React.Component {
     return (
       <section className="infomain-container">
         <div>
-          <button className="infomain-back" onClick={this.props.toggleInfo}>&lt;&nbsp;Вернуться</button>
+          <button className="infomain-back" onClick={this.props.toggleInfo}>&lt;&nbsp;&nbsp;Вернуться</button>
         </div>
         <div>
           <p className="infomain-text">{this.props.info}</p>
